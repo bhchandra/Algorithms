e2bd3b156ded679159b4427f27b009661bee2cb8
Mitra <bhanumchandra@gmail.com>
2017-05-28 16:02:46 -0500

    Adding permutation client which reads in a sequence of strings from standard input and prints exactly k of them, uniformly at random.

---
+++ src/main/java/DequeRandomizedQueue/Permutation.java
@@ -0,0 +1,29 @@
+package DequeRandomizedQueue;
+
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ *
+ * @author MITRA
+ */
+public class Permutation {
+
+    public static void main(String[] args) {
+        final RandomizedQueue<String> rq = new RandomizedQueue<>();
+        
+        final int k = Integer.parseInt(args[0]);
+       
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            rq.enqueue(s);
+        }
+
+        int count = 0;
+        for (String s : rq) {
+            System.out.println(s);
+            if (++count == k) {
+                break;
+            }
+        }
+    }
+}
